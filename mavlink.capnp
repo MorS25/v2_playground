e004c572732a3c528e72c7a4b43dd88d2fcc2abe
Lorenz Meier <lm@inf.ethz.ch>
2015-08-18 11:23:24 +0200

    First capnproto test. attitude grows from 36 to 56 bytes - duh!

---
+++ mavlink.capnp
@@ -0,0 +1,12 @@
+@0x997e5b76f98f279f;
+
+struct Attitude {
+  timestamp @0 :UInt64;
+  w @1 :Float32;
+  x @2 :Float32;
+  y @3 :Float32;
+  z @4 :Float32;
+  rollspeed @5 :Float32;
+  pitchspeed @6 :Float32;
+  yawspeed @7 :Float32;
+}
--- test.cpp
+++ test.cpp
@@ -1,7 +1,31 @@
 
 #include <cstdio>
+#include <iostream>
+#include <fcntl.h>
+#include "mavlink.capnp.h"
+#include <capnp/message.h>
+#include <capnp/serialize-packed.h>
 
 int main(int argc, char *argv[])
 {
 	printf("Hello MAVLink 2.0!\n");
+
+	::capnp::MallocMessageBuilder message;
+
+  	Attitude::Builder attitude = message.initRoot<Attitude>();
+  	attitude.setTimestamp(2384573485);
+  	attitude.setW(0.9f);
+  	attitude.setX(0.23f);
+  	attitude.setY(0.12f);
+  	attitude.setZ(0.09f);
+  	attitude.setRollspeed(0.00023f);
+  	attitude.setPitchspeed(-0.00023f);
+  	attitude.setYawspeed(0.00023f);
+
+  	kj::Array<capnp::word> words = capnp::messageToFlatArray(message);
+  	kj::ArrayPtr<kj::byte> bytes = words.asBytes();
+
+  	int sizeWords = capnp::computeSerializedSizeInWords(message);
+
+  	printf("encoded attitude with %d bytes (vs 36 in MAVLink, duh!)\n", sizeWords * 8);
 }
